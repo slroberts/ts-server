dc4646d9a01f1897e69891a4b016a1d634f8bd44
Shomari Roberts <shomariroberts@gmail.com>
2021-01-08 20:18:29 -0500

    refactored to use routeBinder to generate paths

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,10 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = void 0;
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routeBinder('get');
+exports.put = routeBinder('put');
+exports.post = routeBinder('post');
+exports.del = routeBinder('del');
+exports.patch = routeBinder('patch');
