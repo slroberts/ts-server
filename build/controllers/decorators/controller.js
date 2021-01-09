acd235adafdfb45dcd79b82a8431bd5bcf81b10a
Shomari Roberts <shomariroberts@gmail.com>
2021-01-08 20:32:46 -0500

    remove hard coded strings using enums

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -3,14 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = void 0;
 require("reflect-metadata");
 var AppRouter_1 = require("../../AppRouter");
+var MetadataKeys_1 = require("./MetadataKeys");
 function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
-            var path = Reflect.getMetadata('path', target.prototype, key);
+            var path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key);
+            var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
             if (path) {
-                router.get("" + routePrefix + path, routeHandler);
+                router[method]("" + routePrefix + path, routeHandler);
             }
         }
     };
--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -2,16 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
 require("reflect-metadata");
+var Methods_1 = require("./Methods");
+var MetadataKeys_1 = require("./MetadataKeys");
 function routeBinder(method) {
     return function (path) {
         return function (target, key, desc) {
-            Reflect.defineMetadata('path', path, target, key);
-            Reflect.defineMetadata('method', method, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.path, path, target, key);
+            Reflect.defineMetadata(MetadataKeys_1.MetadataKeys.method, method, target, key);
         };
     };
 }
-exports.get = routeBinder('get');
-exports.put = routeBinder('put');
-exports.post = routeBinder('post');
-exports.del = routeBinder('del');
-exports.patch = routeBinder('patch');
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.post = routeBinder(Methods_1.Methods.post);
+exports.del = routeBinder(Methods_1.Methods.del);
+exports.patch = routeBinder(Methods_1.Methods.patch);
